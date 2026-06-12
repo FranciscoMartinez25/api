07b6e06d411a1bdf2c6e47b8eba8aa3126d81536
Francsico Porfirio Martinez Nuñez <fran2mart5@gmail.com>
2026-06-12 11:47:29 -0300

    Tarjetas y listado de personajes

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,37 +12,45 @@ async function cargarPersonajes(){
     try{
         const respuesta=await fetch(urlTodosPersonaje);
         personajes= await respuesta.json();
-        mostrarPersonajes(personajes);
+        console.log(personajes)
+
+        console.log("----------------------------")
+
+
+
+        contenedor.innerHTML="";
+        console.log(personajes.results)
+        personajes.results.forEach(personaje=>{
+            console.log(personaje)
+            contenedor.innerHTML+=`
+            <div class="card col-lg-3 m-2">
+                <img
+                src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}"
+                alt="${personaje.name}"
+                style="object-fit: cover"
+                 class="card-img-top"
+            >
+                <h3>${personaje.name}</h3>
+                <p><strong>Ocupación:</strong> ${personaje.occupation}</p>
+                <p><strong>Estado:</strong> ${personaje.status}</p>
+                <button data-id="${personaje.id}">
+                Ver detalle
+                </button>
+            </div>
+            `;
+        })
 
     }catch (error){
-        contenedor.innerHTML="";
-
-        lista.forEach(personaje=>{
-            tarjeta.classList.add("card");
-            tarjeta.innerHTML=`
-            <img
-                src="https://cdn.thesimpsonsapi.com/500${personaje.image}"
-                alt="${personaje.name}"
-            >
-
-            <h3>${personaje.name}</h3>
-
-            <p><strong>Ocupación:</strong> ${personaje.occupation}</p>
-
-            <p><strong>Estado:</strong> ${personaje.status}</p>
-
-            <button data-id="${personaje.id}">
-                Ver detalle
-            </button>
-            `;
-            contenedor.appendChild(tarjeta)
-        })
+        
         
     }
+    
     buscacador.addEventListener("input", ()=>{
         const texto=buscacador.value.toLowerCase();
         const filtro=personajes.filter(personaje=>
             personaje.name.toLowerCase().includes(texto)
         );       
     
-    mostrarPersonajes(filtro)});
+    mostrarPersonajes(filtro)
+    })
+};
